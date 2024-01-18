547d2cfa6953cdd1f68b53803dcd4f8e3eb5a8e7
meelunae <git@eleuna.me>
2024-01-18 15:22:33 +0100

    Added more package statuses to smart contract, improved deployer

--- Contract/contracts/Shipment.sol
+++ Contract/contracts/Shipment.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract Shipment {
-  enum DeliveryStatus { SHIPPED, DELIVERED }
+  enum DeliveryStatus { CREATED, PAID, SHIPPED, DELIVERED }
 
   struct Package {
     string packageId;
@@ -20,24 +20,100 @@ contract Shipment {
 
   event Funded(address indexed sender, uint256 amount);
   event PackageCreated(string packageId, address sender, address receiver, uint256 value);
-  event DeliveryStatusUpdated(string packageId, DeliveryStatus status);
+  event PackagePaidFor(string packageId);
+  event PackageShipped(string packageId);
   event PackageDelivered(string packageId, address sender, uint256 amount);
+  event DeliveryStatusUpdated(string packageId, DeliveryStatus status);
+
 
   // This modifier is required so that only the owners of the contract can access informations about packages that are shipped
-   modifier onlyOwner() {
+  modifier onlyOwner() {
     require(msg.sender == owner, "Only the owner can call this function");
     _;
   }
+
+  modifier onlyBuyer(string memory _packageId) {
+      for (uint i = 0; i < packages.length; i++) {
+        if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
+          require(msg.sender == packages[i].receiver, "Only the package receiver can call this function");
+          _; 
+          return;
+        }
+      }
+      revert("Package not found");
+  }
+
+  modifier onlySeller(string memory _packageId) {
+      for (uint i = 0; i < packages.length; i++) {
+        if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
+          require(msg.sender == packages[i].sender, "Only the package sender can call this function");
+          _; 
+          return;
+        }
+      }
+      revert("Package not found");
+    }
 
   constructor() public {
     owner = msg.sender;
   }
 
-  // This function is called to save a newly-shipped package on chain. 
-  function shipPackage(string memory _packageId, address _receiver, uint256 _amount) public {
-      Package memory newPackage = Package(_packageId, DeliveryStatus.SHIPPED, msg.sender, _receiver, _amount);
+  // This function is called to save a newly-created package on chain. 
+  function createPackage(string memory _packageId, address _receiver, uint256 _amount) public {
+      Package memory newPackage = Package(_packageId, DeliveryStatus.CREATED, msg.sender, _receiver, _amount);
       packages.push(newPackage);
+      emit DeliveryStatusUpdated(_packageId, DeliveryStatus.CREATED);
       emit PackageCreated(_packageId, msg.sender, _receiver, _amount);
+      return;
+  }
+
+  // Marking the package as shipped
+  function shipPackage(string memory _packageId) public onlySeller(_packageId) {
+    for (uint i = 0; i < packages.length; i++) {
+      if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
+          require(packages[i].status == DeliveryStatus.PAID, "Package is not in the correct status");
+          packages[i].status = DeliveryStatus.SHIPPED;
+          emit DeliveryStatusUpdated(_packageId, DeliveryStatus.SHIPPED);
+          emit PackageShipped(_packageId);
+          return;
+      }
+    }
+    revert("Package not found");
+  }
+
+  // Payment from receiver to contract
+  function payForPackage(string memory _packageId) public payable onlyBuyer(_packageId) {
+    for (uint i = 0; i < packages.length; i++) {
+      if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
+        require(packages[i].status == DeliveryStatus.CREATED, "Package is not in the correct status");
+        require(msg.value == packages[i].amount, "Incorrect payment amount");
+        packages[i].status = DeliveryStatus.PAID;
+        emit DeliveryStatusUpdated(_packageId, DeliveryStatus.PAID);
+        emit PackagePaidFor(_packageId);
+        return;
+      }
+    }
+    revert("Package not found");
+  }
+
+    // Function responsible for package delivery and payouts
+  function deliverPackage(string memory _packageId) public payable onlyBuyer(_packageId) {
+    for (uint i = 0; i < packages.length; i++) {
+      if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
+        require(packages[i].status == DeliveryStatus.SHIPPED, "Package has not been shipped yet, can't be scanned!");
+        require(packages[i].status != DeliveryStatus.DELIVERED, "Package has already been delivered");
+        
+        address payable sender = address(uint160(packages[i].sender));
+        sender.transfer(packages[i].amount);
+
+        // Update package status
+        packages[i].status = DeliveryStatus.DELIVERED;
+        emit DeliveryStatusUpdated(_packageId, DeliveryStatus.DELIVERED);
+        emit PackageDelivered(_packageId, packages[i].sender, packages[i].amount);
+        return;
+      }
+    }
+    revert("Package not found");
   }
 
   function getAllPackages() public view onlyOwner returns (Package[] memory) {
@@ -54,38 +130,15 @@ contract Shipment {
   }
 
 
-      // Fallback function allows the contract to receive Ether
+    // START OF FUNCTIONS TO HANDLE CONTRACT FUNDING
+
+    // Fallback function allows the contract to receive Ether
     function() external payable {
-        // You can handle the received Ether here if needed
     }
 
     // Function to allow the owner to send funds to the contract
     function fundContract() external payable onlyOwner {
         require(msg.value > 0, "Sent value must be greater than 0");
-        
-        // You can add custom logic here to handle the received funds
         emit Funded(msg.sender, msg.value);
     }
-
-  // Function responsible for package delivery and payouts
-  function deliverPackage(string memory _packageId) public payable {
-    for (uint i = 0; i < packages.length; i++) {
-      if (keccak256(abi.encodePacked(packages[i].packageId)) == keccak256(abi.encodePacked(_packageId))) {
-        require(msg.sender == packages[i].receiver, "Only the receiver can mark the package as delivered");
-        require(packages[i].status != DeliveryStatus.DELIVERED, "Package has already been delivered");
-        
-        address payable sender = address(uint160(packages[i].sender));
-        sender.transfer(packages[i].amount);
-
-        // Update package status
-        packages[i].status = DeliveryStatus.DELIVERED;
-        emit DeliveryStatusUpdated(_packageId, DeliveryStatus.DELIVERED);
-        emit PackageDelivered(_packageId, packages[i].sender, packages[i].amount);
-        
-        return;
-      }
-    }
-    revert("Package not found");
-  }
-
 }
--- Contract/deployer.js
+++ Contract/deployer.js
@@ -1,5 +1,39 @@
 const { exec } = require('child_process');
 const { Web3 } = require('web3');
+const { program } = require('commander');
+
+
+program
+  .version('1.0.0')
+  .description('Script to automate a working setup for a demo SafeDelivery deploy.');
+
+  program
+  .requiredOption('-o, --contract-owner <owner>', 'Specify the contract owner')
+  .requiredOption('-f, --contract-funder <funder>', 'Specify the contract funder')
+  .requiredOption('-s, --sender <sender>', 'Specify the sender')
+  .requiredOption('-r, --receiver <receiver>', 'Specify the receiver')
+  .requiredOption('-p, --package-id <packageId>', 'Specify the package ID');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+// Access the values of the options
+const {
+  contractOwner,
+  contractFunder,
+  sender,
+  receiver,
+  packageId
+} = options;
+
+// Your script logic here...
+console.log('Contract Owner:', contractOwner);
+console.log('Contract Funder:', contractFunder);
+console.log('Sender:', sender);
+console.log('Receiver:', receiver);
+console.log('Package ID:', packageId);
+
 
 const truffleMigrate = () => {
   return new Promise((resolve, reject) => {
@@ -26,28 +60,34 @@ const runTruffleCommands = async (data) => {
     const web3 = new Web3('http://localhost:7545');
     const Shipment = new web3.eth.Contract(ShipmentArtifact.abi, ShipmentArtifact.networks['5777'].address);
 
-    console.log('Contract Address:', await Shipment.options.address);
-    console.log('Balance:', await web3.eth.getBalance(Shipment.options.address));
+    console.log('[i] Contract Address:', await Shipment.options.address);
+    console.log('[i] Balance:', await web3.eth.getBalance(Shipment.options.address));
     
-    console.log("Funding Smart Contract...");
-    const valueToSend = web3.utils.toWei("10", "ether");
+    console.log("[i] Funding Smart Contract...");
     await web3.eth.sendTransaction({
       from: contractFunder,
       to: Shipment.options.address,
-      value: valueToSend,
+      value: web3.utils.toWei("10", "ether"),
     });
-    console.log('New balance:', await web3.eth.getBalance(Shipment.options.address));
+    console.log('[+] New balance:', await web3.eth.getBalance(Shipment.options.address));
 
-    console.log('Executing commands...');
-    await Shipment.methods.shipPackage(packageId, receiver, web3.utils.toWei("2.3", "ether")).send({ from: contractOwner, gas: 500000});
-    console.log("Package shipped successfully.");
+    console.log('[i] Executing commands...\n');
+    console.log("[i] Creating a new package...");
+    await Shipment.methods.createPackage(packageId, receiver, web3.utils.toWei("2.3", "ether")).send({ from: sender, gas: 500000});
+    console.log("[i] Package creation should be completed. Trying to fetch all packages.");
     const result = await Shipment.methods.getAllPackages().call({from: contractOwner});
-    console.log("Fetched all packages. Output is: ", result);
-    console.log('Commands executed successfully.');
+    console.log("[+] Output is:", result);
+    console.log("\n");
+    console.log("[i] Paying for package using the receiver's chain account...");
+    await Shipment.methods.payForPackage(packageId).send({from: receiver, value: web3.utils.toWei("2.3", "ether"), gas: 500000});
+    console.log("[+] Package should be paid for, new output for getAllPackages is: ", await Shipment.methods.getAllPackages().call({from: contractOwner}));
+    console.log("[i] Marking package as shipped through the sender's account...");
+    await Shipment.methods.shipPackage(packageId).send({from: sender, gas: 500000});
+    console.log("[+] Package should be now marked as shipped, new output for getAllPackages is: ", await Shipment.methods.getAllPackages().call({from: contractOwner}));
+    console.log('[+] All commands executed successfully! Scan the package NFC tag with the IoT device to mark it as delivered.');
   } catch (error) {
-    console.error(error);
+    console.error("[-] ", error);
   }
 };
 
-const [, , contractOwner, contractFunder, receiver, packageId] = process.argv;
-runTruffleCommands(contractOwner, receiver, contractFunder, packageId);
+runTruffleCommands(contractOwner, contractFunder, sender, receiver, contractFunder, packageId);
